7f0f0988e3b8d938bcda78ef9f669f993b953d11
David Goeke <dg@github.dgoeke.io>
2020-06-09 14:13:39 -0700

    Logging around migrations

--- lib/tsheeter/release.ex
+++ lib/tsheeter/release.ex
@@ -1,11 +1,15 @@
 defmodule Tsheeter.Release do
+  require Logger
   @app :tsheeter
 
   def migrate do
     load_app()
 
     for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+      Logger.info "Migrating: #{inspect(repo, pretty: true)}"
+      {:ok, a, b} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+      Logger.info "a: #{inspect(a, pretty: true)}"
+      Logger.info "b: #{inspect(b, pretty: true)}"
     end
   end
 
