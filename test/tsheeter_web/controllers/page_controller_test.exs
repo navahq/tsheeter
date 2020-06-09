625425a95b61b413a29bdb50b3a973690dc49336
David Goeke <dg@github.dgoeke.io>
2020-06-09 15:33:33 -0700

    Add placeholder landing page

--- test/tsheeter_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TsheeterWeb.PageControllerTest do
-  use TsheeterWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
