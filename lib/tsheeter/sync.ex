b3f95c459e87ee4cb678e057521858412358c40f
David Goeke <dg@github.dgoeke.io>
2020-06-11 16:12:12 -0700

    Move db logic into token.ex

--- lib/tsheeter/sync.ex
+++ lib/tsheeter/sync.ex
@@ -1,13 +1,10 @@
 defmodule Tsheeter.Sync do
   use GenServer
   alias Tsheeter.Oauther
-  alias Tsheeter.Repo
   alias Tsheeter.Token
   require Logger
-  import Ecto.Query, only: [from: 2]
 
   @refresh_schedule 1_000 * 10 * 60   # (in ms) scan for tokens to refresh every 10 minutes
-  @refresh_period   60 * 60 * 24      # (in seconds) refresh tokens 24 hours before they expire
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, :initial_state)
@@ -15,7 +12,7 @@ defmodule Tsheeter.Sync do
 
   def init(_) do
     Oauther.subscribe()
-    send(self(), :refresh)
+    schedule_refresh()
     {:ok, :no_state}
   end
 
@@ -39,25 +36,14 @@ defmodule Tsheeter.Sync do
       refresh_token: refresh_token
     }
 
-    %Token{}
-    |> Token.changeset(attrs)
-    |> Repo.insert!(
-      on_conflict: [
-        set: [access_token: access_token, refresh_token: refresh_token, expires_at: expires_at]
-      ],
-      conflict_target: :slack_uid
-    )
-
+    Token.insert!(attrs)
     {:noreply, state}
   end
 
   def handle_info(_, state), do: {:noreply, state}
 
   def refresh_tokens() do
-    expire_max = DateTime.utc_now() |> DateTime.add(@refresh_period)
-    tokens = Repo.all(from t in Token, where: t.expires_at <= ^expire_max)
-
-    for token <- tokens do
+    for token <- Token.all_expiring() do
       Oauther.create(token.slack_uid)
       Oauther.refresh(token.slack_uid, token.access_token, token.refresh_token)
     end
--- lib/tsheeter/token.ex
+++ lib/tsheeter/token.ex
@@ -1,6 +1,11 @@
 defmodule Tsheeter.Token do
   use Ecto.Schema
+  alias Tsheeter.Repo
+  alias Tsheeter.Token
   import Ecto.Changeset
+  import Ecto.Query, only: [from: 2]
+
+  @expire_period 60 * 60 * 24   # (in seconds) refresh tokens 24 hours before they expire
 
   schema "tokens" do
     field :access_token, :string
@@ -18,5 +23,29 @@ defmodule Tsheeter.Token do
     |> cast(attrs, [:slack_uid, :tsheets_uid, :access_token, :refresh_token, :expires_at])
     |> validate_required([:slack_uid, :tsheets_uid, :access_token, :refresh_token, :expires_at])
     |> unique_constraint(:slack_uid)
+  end
+
+  def get_by_slack_id(slack_id) do
+    query = from t in Token, where: t.slack_uid == ^slack_id
+    case Repo.all(query) do
+      [item] -> item
+      [] -> nil
+    end
+  end
+
+  def all_expiring() do
+    expire_max = DateTime.utc_now() |> DateTime.add(@expire_period)
+    Repo.all(from t in Token, where: t.expires_at <= ^expire_max)
+  end
+
+  def insert!(attrs = %{access_token: access_token, expires_at: expires_at, refresh_token: refresh_token}) do
+    %Token{}
+    |> Token.changeset(attrs)
+    |> Repo.insert!(
+      on_conflict: [
+        set: [access_token: access_token, refresh_token: refresh_token, expires_at: expires_at]
+      ],
+      conflict_target: :slack_uid
+    )
   end
 end
